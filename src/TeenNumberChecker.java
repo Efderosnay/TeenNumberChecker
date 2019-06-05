95aefa23516a7f639338215c9040e0697f7646cc
Eugenie Fromet de Rosnay <e.fderosnay@gmail.com>
2019-06-05 12:08:21 +0100

    Initial commit

---
+++ src/TeenNumberChecker.java
@@ -0,0 +1,13 @@
+public class TeenNumberChecker {
+    public static boolean hasTeen (int num1, int num2, int num3){
+        if((num1 > 12 && num1 < 20) || (num2 > 12 && num2  < 20) || (num3 > 12 && num3  < 20)){
+            return true;
+        } return false;
+    }
+
+    public static boolean isTeen (int nums){
+        if (nums > 12 && nums  < 20){
+            return true;
+        } return false;
+    }
+}
